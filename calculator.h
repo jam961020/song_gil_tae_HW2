300c35b5208894ff4c8d5189a17fdd51ece81995
jam961020 <48705681+jam961020@users.noreply.github.com>
2019-04-09 16:11:48 +0900

    Create calculator.h

---
+++ calculator.h
@@ -0,0 +1,13 @@
+double Addfunc(double val1,double val2){
+	return val1 + val2;
+}
+double Minus(double val1,double val2){
+	return val1 - val2;
+}
+double Multifunc(double val1,double val2){
+	return val1*val2;
+}
+
+double Dvide(double val1,double val2){
+	return val1/val2;
+}		
